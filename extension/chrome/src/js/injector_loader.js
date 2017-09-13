9daf19ffef02cb73e29df843026f20a385117d41
Pavel Vanecek <pavel.vanecek@merck.com>
2017-09-13 14:53:20 +0200

    Show error when extension initialization fails

--- extension/chrome/src/js/injector_loader.js
+++ extension/chrome/src/js/injector_loader.js
@@ -55,7 +55,7 @@ function injectEngine(){
 
 		// css
 		injectCssFile('css/page_injection.css');
-	});
+	}, console.error.bind(console, "Loading failed"));
 }
 
 function injectBitbucketDetector() {
